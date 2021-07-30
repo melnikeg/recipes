473a52976f6d22e210600fa679dcf07615d8adab
unknown <Melnik Egor>
2021-07-30 12:22:52 +0300

    Change Summary.js, Recipe.js

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,29 +1,8 @@
-import { createClass, PropTypes } from 'react';
-
-export const Summary = createClass({
-    displayName: "Summary",
-    propTypes:{
-        ingredients: PropTypes.array.isRequired,
-        steps: PropTypes.array.isRequired,
-        title: PropTypes.string
-    },
-    getDefaultProps() {
-        return {
-            ingredients: 0,
-            steps: 0,
-            title: "[recipe]"
-        }
-    },
-    render() {
-        const {ingredients, steps, title} = this.props
-        return (
-            <div className="summary">
-                <h1>{title}</h1>
-                <p>
-                    <span>{ingredients.length} Ingredients</span> |
-                    <span>{steps.length} Steps</span>
-                </p>
-            </div>
-        )
-    }
-})
+export const Summary = (props) => 
+        <div className="summary">
+            <h1>{props.title}</h1>
+            <p>
+                <span>{props.ingredients.length} Ingredients</span> |
+                <span>{props.steps.length} Steps</span>
+            </p>
+        </div>   
