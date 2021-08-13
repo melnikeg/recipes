680c5a83bd27a82ff9544b68f01ee4d13ae6e94f
Melnik Egor, Xubuntu <melnikeg@ukr.net>
2021-08-13 13:26:29 +0300

    Change Summary. Latest commit

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,8 +1,30 @@
-export const Summary = (props) => 
-        <div className="summary">
-            <h1>{props.title}</h1>
-            <p>
-                <span>{props.ingredients.length} Ingredients</span> |
-                <span>{props.steps.length} Steps</span>
-            </p>
-        </div>   
+import React from "react";
+import { PropTypes } from "react";
+
+export class Summary extends React.Component {
+    
+    propTypes: {
+        ingredients: PropTypes.array.isRequired,
+        steps: PropTypes.array.isRequired        
+    }
+
+    getDefaultProps() {
+        return {
+            ingredients: 0,
+            steps: 0,
+            title: ["recipe"]
+        }
+    }
+    
+    render() {
+        return (
+            <div className="summary">
+                <h1>{this.props.title}</h1>
+                <p>
+                    <span>{this.props.ingredients.length} Ingredients</span> |
+                    <span>{this.props.steps.length} Steps</span>
+                </p>
+            </div>
+        )
+    }
+}
